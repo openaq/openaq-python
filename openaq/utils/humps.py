de107888601ec5a70371342e7cf79db546990f2e
Gabriel Fosse <kraken+majesticio@users.noreply.github.com>
2023-12-07 11:03:31 -0800

    Vendor humps.py for camel case and snake case conversion

---
+++ openaq/utils/humps.py
@@ -0,0 +1,60 @@
+# This implementation of camelize and decamelize functions is adapted from the humps library by Nick Ficano.
+# Original source: https://github.com/nficano/humps
+# The humps library is used for converting strings into different naming conventions (e.g., camelCase, PascalCase, snake_case).
+# Thanks to Nick Ficano for the original implementation.
+
+import re
+
+from collections.abc import Mapping
+
+ACRONYM_RE = re.compile(r"([A-Z\d]+)(?=[A-Z\d]|$)")
+PASCAL_RE = re.compile(r"([^\-_]+)")
+SPLIT_RE = re.compile(r"([\-_]*(?<=[^0-9])(?=[A-Z])[^A-Z]*[\-_]*)")
+UNDERSCORE_RE = re.compile(r"(?<=[^\-_])[\-_]+[^\-_]")
+
+
+def camelize(str_or_iter):
+    """
+    Convert a string, dict, or list of dicts to camel case.
+
+    :param str_or_iter:
+        A string or iterable.
+    :type str_or_iter: Union[list, dict, str]
+    :rtype: Union[list, dict, str]
+    :returns:
+        camelized string, dictionary, or list of dictionaries.
+    """
+    if isinstance(str_or_iter, (list, Mapping)):
+        return _process_keys(str_or_iter, camelize)
+
+    s = _is_none(str_or_iter)
+    if s.isupper() or s.isnumeric():
+        return str_or_iter
+
+    if len(s) != 0 and not s[:2].isupper():
+        s = s[0].lower() + s[1:]
+
+    # For string "hello_world", match will contain
+    #             the regex capture group for "_w".
+    return UNDERSCORE_RE.sub(lambda m: m.group(0)[-1].upper(), s)
+
+
+def decamelize(str_or_iter):
+    """
+    Convert a string, dict, or list of dicts to snake case.
+
+    :param str_or_iter:
+        A string or iterable.
+    :type str_or_iter: Union[list, dict, str]
+    :rtype: Union[list, dict, str]
+    :returns:
+        snake cased string, dictionary, or list of dictionaries.
+    """
+    if isinstance(str_or_iter, (list, Mapping)):
+        return _process_keys(str_or_iter, decamelize)
+
+    s = _is_none(str_or_iter)
+    if s.isupper() or s.isnumeric():
+        return str_or_iter
+
+    return _separate_words(_fix_abbreviations(s)).lower()
