ab7e20e7fa6a52b0c818cb586390c2c2604ca3a9
Russ Biggs <russbiggs@gmail.com>
2024-08-02 15:05:33 -0600

    adds methods and tests for v3 measurements with new rollup options

--- openaq/_sync/models/measurements.py
+++ openaq/_sync/models/measurements.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import datetime
-from typing import List, Union
+from typing import Union
 
+from openaq.shared.exceptions import NotFoundError
 from openaq.shared.models import build_query_params
 from openaq.shared.responses import MeasurementsResponse
+from openaq.shared.types import Rollup, Data
 
 from .base import SyncResourceBase
 
@@ -14,32 +16,31 @@ class Measurements(SyncResourceBase):
 
     def list(
         self,
-        locations_id: int,
+        sensors_id: int,
         date_from: Union[datetime.datetime, str, None] = "2016-10-10",
         date_to: Union[datetime.datetime, str, None] = None,
+        data: Union[Data, None] = None,
+        rollup: Union[Rollup, None] = None,
         page: int = 1,
         limit: int = 1000,
-        parameters_id: Union[int, List[int], None] = None,
     ) -> MeasurementsResponse:
         """List air quality measurements based on provided filters.
 
-        Provides the ability to filter the measurements resource by location, date range,
-        pagination settings, and specific parameters.
+        Provides the ability to filter the measurements resource by sensor, date range,
+        pagination settings.
 
-        * `locations_id` - Filters measurements to a specific locations ID (required)
+        * `sensors_id` - Filters measurements to a specific sensors ID (required)
         * `date_from` - Declare a start time for data retrieval
         * `date_to` - Declare an end time or data retrieval
         * `page` - Specifies the page number of results to retrieve
         * `limit` - Sets the number of results generated per page
-        * `parameters_id` - Filters results by selected parameters ID(s)
 
         Args:
-            locations_id: The ID of the location for which measurements should be retrieved.
+            sensors_id: The ID of the sensor for which measurements should be retrieved.
             date_from: Starting date for the measurement retrieval. Can be a datetime object or ISO-8601 formatted date or datetime string.
             date_to: Ending date for the measurement retrieval. Can be a datetime object or ISO-8601 formatted date or datetime string.
             page: The page number to fetch. Page count is determined by total measurements found divided by the limit.
             limit: The number of results returned per page.
-            parameters_id: Single parameters ID or an array of IDs.
 
         Returns:
             MeasurementsResponse: An instance representing the list of retrieved air quality measurements.
@@ -57,13 +58,27 @@ class Measurements(SyncResourceBase):
         params = build_query_params(
             page=page,
             limit=limit,
-            locations_id=locations_id,
             date_from=date_from,
             date_to=date_to,
-            parameters_id=parameters_id,
         )
+        base_path = f'/sensors/{sensors_id}'
+        if data == 'measurements' or data == None:
+            path = base_path + '/measurements'
+        if data == 'hours':
+            path = base_path + '/hours'
+        if data == 'days':
+            path = base_path + '/days'
+        if data == 'years':
+            path = base_path + '/years'
+        if rollup:
+            path += f'/{rollup}'
+        if data == 'hours' and rollup == 'hourly':
+            raise NotFoundError()
+        if data == 'days' and rollup == 'daily':
+            raise NotFoundError()
+        if data == 'years' and rollup == 'yearly':
+            raise NotFoundError()
 
-        measurements_response = self._client._get(
-            f"/locations/{locations_id}/measurements", params=params
-        )
+        measurements_response = self._client._get(path, params=params)
+
         return MeasurementsResponse.read_response(measurements_response)
