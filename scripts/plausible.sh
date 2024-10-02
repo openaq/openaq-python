61ce3bcfbad2b7869d1b7b00b2222fc4531884f1
Russ Biggs <russbiggs@gmail.com>
2024-10-02 09:46:41 -0600

    script update

--- scripts/plausible.sh
+++ scripts/plausible.sh
@@ -11,8 +11,8 @@ find "$SEARCH_DIR" -type f -name "*.html" | while read -r html_file; do
     if grep -q "$OLD_STRING" "$html_file"; then
         echo "Updating $html_file"
 
-        sed -i "s|$OLD_STRING|$NEW_STRING|g" "$html_file"
+        sed -i "s|src=\"\([^\"']*\)$OLD_STRING|src=\"$NEW_STRING|g" "$html_file"
 
-        sed -i "/<script[^>]*src=\"${NEW_STRING}\"/i $PRECONNECT_LINK" "$html_file"
+        sed -i "/<script[^>]*src=\"$NEW_STRING\"/i $PRECONNECT_LINK" "$html_file"
     fi
 done
