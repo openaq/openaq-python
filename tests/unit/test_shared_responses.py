2cdfa42ac47d0d5f2958392883318da0ebd98d6a
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2026-01-09 18:50:50 +0100

    fix: data type issues (#61) (#62)
    * chore: fix typo in docs (#60)
    
    * fix: added __post_init__ calls to Latest, LatestBase and Sensor class
    
    Post init calls are added to these three classes to fix field type
    issues.
    Related to #61
    
    * fix: fixed issue with unit tests and added missing avg field to Summary
    
    Unit test were failing now that we are loading Sensors properly.
    Cases got exposed where a field is missing completely in the response json
    (for example description of a Parameter), but when we read the response,
    convert back to json and parse again, a field gets added with a value None
    ('description': None) which causes the assert to fail. This issue is
    now bypassed by using a function that removes None key value pairs in the
    nested structure from both arguments of the assert call.
    
    Another issue that was exposed with the Sensor test was that the avg field
    wasnt loaded because it was not a part of our Summary class. When i tried
    simply adding it as avg: float - the tests started to fail, now on the
    Measurement values. This was happening because those get Summary values
    from responses that don't have the avg field. In order to satisfy both
    I defined the field as avg: float | None = None. @russbigs please review
    this and tell if it is the intended behavior.
    
    Related to #61
    
    * test: added a test to check field types of resources
    
    Added a test to check if the fields of resource classes have been
    initialized correctly. The helper function value_matches_type that
    checks if the runtime values are of correct type covers all the special
    cases we have in the code right now (Union, numbers, lists with same
    type values, tuples), writing it in a fully generic way is not trivial.
    
    This also exposed a type issue with Location.bounds field which is now
    converted to appropriate type (tuple) in a __post_init__ call.
    
    Related to #61
    
    ---------
    
    Co-authored-by: Mueez Khan <30333942+rzmk@users.noreply.github.com>

--- tests/unit/test_shared_responses.py
+++ tests/unit/test_shared_responses.py
@@ -2,6 +2,12 @@ import json
 from pathlib import Path
 
 import httpx
+
+from dataclasses import fields
+from typing import get_type_hints, get_origin, Union, get_args
+import numbers
+import types
+
 import pytest
 
 from openaq.shared.responses import (
@@ -69,6 +75,49 @@ def mock_response(data: str) -> httpx.Response:
         status_code=200, headers=RATE_LIMIT_HEADERS, json=json.loads(data)
     )
 
+def remove_nulls(value):
+    if isinstance(value, dict):
+        return {k: remove_nulls(v) for k, v in value.items() if v is not None}
+    if isinstance(value, list):
+        return [remove_nulls(v) for v in value]
+    else:
+        return value
+
+def value_matches_type(value, expected_type) -> bool:
+    """Currently has some special cases covered(Union, numbers, lists with same 
+    type values, tuples), but making this test fully generic is not trivial"""
+    if expected_type is float:
+        return isinstance(value, numbers.Real)
+
+    if expected_type is int:
+        return isinstance(value, numbers.Integral)
+
+    origin = get_origin(expected_type)
+    if origin is Union or origin is types.UnionType:   
+        return any(value_matches_type(value, t) for t in get_args(expected_type))
+    
+    if origin is not None and not isinstance(value, origin):
+        return False
+
+    args = get_args(expected_type)
+
+    if origin is list:
+        if not isinstance(value, list):
+            return False
+
+        (item_type,) = args
+
+        return all(value_matches_type(v, item_type) for v in value) 
+    
+    if origin is tuple and len(args) == 2 and args[1] is Ellipsis:
+        return all(value_matches_type(v, args[0]) for v in value)
+
+    if origin is tuple:
+        return len(value) == len(args) and all(
+            value_matches_type(v, t) for v, t in zip(value, args)
+        )
+
+    return isinstance(value, expected_type)
 
 @pytest.mark.respx(base_url="https://api.openaq.org/v3/")
 def test_rate_limit_headers_response():
@@ -138,7 +187,7 @@ def test_responses_validation(name: str, response_class: _ResponseBase):
 @pytest.mark.parametrize(
     "name,response_class",
     [
-        # ('measurements', MeasurementsResponse),
+        ('measurements', MeasurementsResponse),
         ('countries', CountriesResponse),
         ('locations', LocationsResponse),
         ('providers', ProvidersResponse),
@@ -159,7 +208,7 @@ def test_responses_json(name: str, response_class: _ResponseBase):
     d = json.loads(response_data.json())
     headers_less_response = {k: d[k] for k in set(list(d.keys())) - set(['headers'])}
 
-    assert json.loads(response) == headers_less_response
+    assert remove_nulls(json.loads(response)) == remove_nulls(headers_less_response)
 
 
 def test_response_ignores_unexpected_fields():
@@ -176,3 +225,35 @@ def test_response_ignores_unexpected_fields():
         ), "Unexpected 'anotherField' was not ignored"
     except Exception as e:
         pytest.fail(f"Deserialization failed with unexpected field 'anotherField': {e}")
+
+@pytest.mark.parametrize(
+    "name,response_class",
+    [
+        ('measurements', MeasurementsResponse),
+        ('countries', CountriesResponse),
+        ('locations', LocationsResponse),
+        ('providers', ProvidersResponse),
+        ('parameters', ParametersResponse),
+        ('instruments', InstrumentsResponse),
+        ('owners', OwnersResponse),
+        ('manufacturers', ManufacturersResponse),
+        ('locations_variation', LocationsResponse),
+        ('sensors', SensorsResponse),
+    ],
+)
+
+@pytest.mark.respx(base_url="https://api.openaq.org/v3/")
+def test_field_types(name: str, response_class: _ResponseBase):
+    """Tests whether all fields have the correct types"""
+    response = read_response_file(name)
+    mocked = mock_response(response)
+    response_data = response_class.read_response(mocked)
+
+    for response_data in response_data.results:
+        type_hints = get_type_hints(type(response_data))
+
+        for field in fields(response_data):
+            value = getattr(response_data, field.name)
+            expected_type = type_hints.get(field.name)
+
+            assert value_matches_type(value, expected_type)
