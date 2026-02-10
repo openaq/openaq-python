614ec6488748219c4dac05a00eee5448cbcd8d72
Russ Biggs <russbiggs@gmail.com>
2026-02-10 13:55:01 -0700

    add new validators for data,rollup measurements

--- openaq/shared/types.py
+++ openaq/shared/types.py
@@ -1,16 +1,29 @@
 """Shared custom types."""
 
-from typing import Literal, TypeAlias, TypedDict
+from typing import Literal, TypeAlias, TypedDict, get_args
 
+_ROLLUP_VALUES = (
+    'hourly',
+    'daily',
+    'monthly',
+    'yearly',
+    'hourofday',
+    'dayofweek',
+    'monthofyear',
+)
 Rollup: TypeAlias = Literal[
     'hourly', 'daily', 'monthly', 'yearly', 'hourofday', 'dayofweek', 'monthofyear'
 ]
 
 Data: TypeAlias = Literal['measurements', 'hours', 'days', 'years']
+_DATA_VALUES = get_args(Data)
 
 ParameterType: TypeAlias = Literal['pollutant', 'meteorological']
+_PARAMETER_TYPE_VALUES = get_args(ParameterType)
 
 SortOrder: TypeAlias = Literal['ASC', 'DESC', 'asc', 'desc']
+_SORT_ORDER_VALUES = get_args(SortOrder)
+
 
 CoordinatesRadius: TypeAlias = tuple[tuple[float, float], int, None]
 
