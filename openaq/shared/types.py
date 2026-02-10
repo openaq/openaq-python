be1bfc0332f76ad1a6d455bbc4f19c4181dc985f
Russ Biggs <russbiggs@gmail.com>
2026-02-10 12:33:50 -0700

    remove unused types

--- openaq/shared/types.py
+++ openaq/shared/types.py
@@ -2,26 +2,14 @@
 
 from typing import Literal, TypeAlias, TypedDict
 
-_ROLLUP_VALUES = (
-    'hourly',
-    'daily',
-    'monthly',
-    'yearly',
-    'hourofday',
-    'dayofweek',
-    'monthofyear',
-)
 Rollup: TypeAlias = Literal[
     'hourly', 'daily', 'monthly', 'yearly', 'hourofday', 'dayofweek', 'monthofyear'
 ]
 
-_DATA_VALUES = ('measurements', 'hours', 'days', 'years')
 Data: TypeAlias = Literal['measurements', 'hours', 'days', 'years']
 
-_PARAMETER_TYPE_VALUES = ('pollutant', 'meteorological')
 ParameterType: TypeAlias = Literal['pollutant', 'meteorological']
 
-_SORT_ORDER_VALUES = ('ASC', 'DESC', 'asc', 'desc')
 SortOrder: TypeAlias = Literal['ASC', 'DESC', 'asc', 'desc']
 
 CoordinatesRadius: TypeAlias = tuple[tuple[float, float], int, None]
